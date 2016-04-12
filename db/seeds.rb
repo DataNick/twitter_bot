3b4081f4c7a3ae0e11c482911a62533230d27e36
DataNick <DataNick@users.noreply.github.com>
2016-04-12 15:11:01 -0400

    Seed database and ran migrations for bot, response and user tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Response.create(message: "Sending a tweet from twitbottr application.")
